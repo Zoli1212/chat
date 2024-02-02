a602654f472bba287b1931e28d039052dccdc68d
butterfly-react <rdigital4455@gmail.com>
2024-02-02 14:45:51 +0100

    '4'

--- src/providers/layout-components/header.tsx
+++ src/providers/layout-components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { UserType } from "@/interfaces";
 import { getCurrentUserFromMongoDB } from "@/server-actions/users";
-import { Avatar } from "antd";
+import { Avatar, message } from "antd";
 import React, { useEffect, useState } from "react";
 import CurrentUserInfo from "./current-user-info";
 import { usePathname } from "next/navigation";
@@ -9,26 +9,24 @@ import { SetCurrentUser, UserState } from "@/redux/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 function Header() {
 
-  const { currentUserData }: UserState = useSelector((state: any) => state.user)
-
-
-
-  const [showCurrentUserInfo, setShowCurrentUserInfo] = useState<boolean>(false);
-
-    const pathname = usePathname()
-    const dispatch = useDispatch()
-
-    const isPublicRoute = pathname.includes('sign-in') || pathname.includes('sign-out')
-    if(isPublicRoute) return null
+  const pathname = usePathname();
+  const isPublicRoute =
+    pathname.includes("sign-in") || pathname.includes("sign-up");
+  if (isPublicRoute) return null;
+  const dispatch = useDispatch();
+  const { currentUserData }: UserState = useSelector(
+    (state: any) => state.user
+  );
+  const [showCurrentUserInfo, setShowCurrentUserInfo] =
+    React.useState<boolean>(false);
 
   const getCurrentUser = async () => {
     try {
       const response = await getCurrentUserFromMongoDB();
       if (response.error) throw new Error(response.error);
-      dispatch(SetCurrentUser(response as UserType))
-      
-    } catch (error) {
-      console.log((error as Error).message);
+      dispatch(SetCurrentUser(response as UserType));
+    } catch (error: any) {
+      message.error(error.message);
     }
   };
 
--- src/providers/layout-provider.tsx
+++ src/providers/layout-provider.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Header from './layout-components/header'
-import Content from './layout-components/content'
+"use client";
+import React from "react";
+import Header from "./layout-components/header";
+import Content from "./layout-components/content";
 
-function LayoutProvider({ children }: { children: React.ReactNode}) {
+function LayoutProvider({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
-      <Content>
-      { children }
-
-      </Content>
+      <Content>{children}</Content>
     </div>
-  )
+  );
 }
 
-export default LayoutProvider
+export default LayoutProvider;
